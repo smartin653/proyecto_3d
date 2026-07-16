767a1da93a299ef2295908478c49dbcd202735a8
smartin653 <martin@rotgrafik.com>
2026-07-16 10:11:25 -0600

    contraseña

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 import Experience from "./core/experience.js";
 import AccessManager from "./access/AccessManager.js";
 
-// window.addEventListener("DOMContentLoaded", () => {
-
-//     const access = new AccessManager();
-
-//     access.onSuccess(() => {
-
-//         new Experience();
-
-//     });
-
-// });
-
 window.addEventListener("DOMContentLoaded", () => {
+
+    const access = new AccessManager();
+
+    access.onSuccess(() => {
+
         new Experience();
 
+    });
+
 });
+
+// window.addEventListener("DOMContentLoaded", () => {
+//         new Experience();
+
+// });
