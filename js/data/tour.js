f63d5789fef89510006ff4c436e415dafa7de933
smartin653 <martin@rotgrafik.com>
2026-08-03 16:27:07 -0600

    dates

--- js/data/tour.js
+++ js/data/tour.js
@@ -12,81 +12,10 @@ export default {
       country: "Colombia",
       venue: "Festival Cordillera",
       presale: "YA EN VENTA GENERAL",
-      tickets: "https://www.cordillerafestival.com/entradas"
+      tickets: "#"
     },
 
-    {
-      date: "Sat 09/09/2024",
-      city: "Medellín",
-      country: "Colombia",
-      venue: "Teatro Metropolitano de Medellín",
-      presale: "YA EN VENTA GENERAL",
-      tickets: "https://tuboleta.com/es/eventos/ed-maverick-0"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "Berlin",
-      country: "Germany",
-      venue: "Metropol",
-      presale: "https://www.eventim.de/event/4176844/?affiliate=VUC",
-      tickets: "https://www.eventim.de/artist/ed-maverick/ed-maverick-4176844/"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "Paris",
-      country: "France",
-      venue: "Alhambra",
-      presale: "https://www.ticketmaster.fr/fr/manifestation/ed-maverick-billet/idmanif/669644/codtypadh/FCM/numadh/01/codeconf/FTMS01",
-      tickets: "https://www.livenation.fr/ed-maverick-tickets-adp1600864"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "Milan",
-      country: "Italy",
-      venue: "Santeria Toscana 31",
-      presale: "https://www.ticketone.it/artist/ed-maverick/?affiliate=PI2",
-      tickets: "https://www.ticketone.it/artist/ed-maverick/?affiliate=VIC"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "London",
-      country: "UK",
-      venue: "Islington Assembly Hall",
-      presale: "https://link.dice.fm/Yc921156491f?code=SAERY",
-      tickets: "https://link.dice.fm/D565c09cac4a"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "Madrid",
-      country: "Spain",
-      venue: "Sala La Riviera",
-      presale: "https://tickets.oneboxtds.com/unitedlive/select/2904473?hl=es-ES",
-      tickets: "https://tickets.oneboxtds.com/unitedlive/select/2904473?hl=es-ES"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "Barcelona",
-      country: "Spain",
-      venue: "Sala Razzmatazz",
-      presale: "https://tickets.oneboxtds.com/unitedlive/select/2904460?hl=es-ES",
-      tickets: "https://tickets.oneboxtds.com/unitedlive/select/2904460?hl=es-ES"
-    },
-
-    {
-      date: "Sat 09/09/2024",
-      city: "Hermosillo",
-      country: "México",
-      venue: "Festival Humano",
-      presale: "YA EN VENTA GENERAL",
-      tickets: "https://www.passline.com/eventos/festival-humano-26"
-    }
-
+   
   ]
 
 };
